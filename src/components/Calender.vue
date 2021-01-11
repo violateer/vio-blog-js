6dd04c890931e2ad1f35be93a88222ba1c73fa79
violateer <1828257089@qq.com>
2021-01-11 18:38:01 +0800

    创建calender组件

---
+++ src/components/Calender.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>日历</div>
+</template>
+
+<script lang="js">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Calender'
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="home w">
-    <Card v-for="article in getArticles"
+    <Card v-for="(article,index) in getArticles"
           :title="article.title"
           :author="article.author"
           :content="article.content"
           :review-num="article.reviewNum"
           :page-views="article.pageViews"
           :date="article.createdAt"
+          :key="index"
     />
+    <Calender class="calender"/>
   </div>
 </template>
 
 <script lang="js">
 import Vue from 'vue';
 import Card from '@/components/Card.vue';
+import Calender from '@/components/Calender.vue';
 import { mapGetters } from 'vuex';
 
 export default Vue.extend({
@@ -28,13 +31,11 @@ export default Vue.extend({
     await this.$store.dispatch('articles/actionRequestArticles');
   },
   computed: {
-    // getArticles () {
-    //   console.log(this.$store.getters['articles/watchArticles']);
-    // }
     ...mapGetters({ getArticles: 'articles/watchArticles' })
   },
   components: {
-    Card
+    Card,
+    Calender
   }
 });
 </script>
@@ -42,6 +43,14 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .home {
   margin-top: 60px;
+  position: relative;
+
+  .calender {
+    position: absolute;
+    top: 0;
+    right: 200px;
+    margin-top: 50px;
+  }
 }
 </style>
 
