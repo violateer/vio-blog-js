e28951dfa1f81b8c82ad3c6a3233b9335a7b0e3d
violateer <1828257089@qq.com>
2021-01-14 19:35:45 +0800

    完成上传博客页面

--- src/request/api.js
+++ src/request/api.js
@@ -2,5 +2,8 @@ import request from '@/request/httpRequest';
 
 export default {
     // 获取所有文章
-    getArticles: () => request.get('/api/articles')
+    getArticles: () => request.get('/api/articles'),
+    
+    // 上传文件
+    uploadFile: (url, formData, config) => request.post(url, formData, config)
 };
--- src/views/Blog/BlogUpload.vue
+++ src/views/Blog/BlogUpload.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="wrap">
-    <form action="post" @submit="onSubmit">
-      <label for="title">文章标题</label>
-      <input type="text" id="title">
-      <label for="author">文章作者</label>
-      <input type="text" id="author">
-      <label for="upload">请选择md文件</label>
-      <input type="file" value="" id="upload" @change="uploadConfig">
+    <form action="post" @submit="onSubmit" autocomplete=“off” class="uploadForm">
+      <div class="title">
+        <label for="title">文章标题</label>
+        <input type="text" id="title">
+      </div>
+      <div class="author">
+        <label for="author">文章作者</label>
+        <input type="text" id="author">
+      </div>
+      <div class="upload">
+        <label for="upload">上传文件</label>
+        <input type="file" value="" id="upload" @change="uploadConfig">
+      </div>
       <button type="submit">上传博客</button>
     </form>
   </div>
@@ -47,5 +53,54 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 150px 0;
 
+  .uploadForm {
+    width: 800px;
+    position: relative;
+
+    div {
+      margin-left: 50%;
+      transform: translateX(-50%);
+      display: flex;
+      margin-bottom: 50px;
+
+      #upload {
+        padding-top: 4px;
+        padding-left: 10px;
+        width: 180px;
+      }
+    }
+
+    label {
+      display: inline-block;
+      padding: 3px 6px;
+      text-align: right;
+      width: 100px;
+      vertical-align: top;
+      font-size: 20px;
+    }
+
+
+    button {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-80%);
+      width: 100px;
+      height: 35px;
+      cursor: pointer;
+    }
+  }
+
+  input {
+    outline: none;
+    background-color: #eee;
+    border-color: #eee;
+    width: 185px;
+  }
+}
 </style>
